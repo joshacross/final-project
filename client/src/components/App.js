6bea33c885fa51a341c2663e8b2f90a398a3bdd9
Killa J <joshuaalexandercross@gmail.com>
2021-07-15 00:44:50 -0500

    combining AR with Ecommerce Site using Router and State

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,28 +1,71 @@
-import React from 'react';
-// import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ReactDOM, { render } from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+// import { setContext } from '@apollo/client/link/context';
+
+import Home from '../pages/Home';
+// import Detail from './pages/Detail';
+// import NoMatch from './pages/NoMatch';
+// import Login from './pages/Login';
+// import Signup from './pages/Signup';
+// import Nav from './components/Nav';
+import { StoreProvider } from "../utils/GlobalState";
+// import Success from './pages/Success';
+// import OrderHistory from './pages/OrderHistory';
+import Hiro from './hiro/Hiro';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// const authLink = setContext((_, { headers }) => {
+//   const token = localStorage.getItem('id_token');
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : '',
+//     },
+//   };
+// });
+
+const client = new ApolloClient({
+  // link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 // ifconfig -> en0 -> inet -> https/x:3000
 
-import Hiro from './hiro/Hiro';
+
 // import Torus from './torus/Torus';
 // import RollingScopes from './rollingScopes/RollingScopes';
 // import Earth from './earth/Earth';
 // import TrainTicket from './trainTicket/TrainTicket';
 // import Railcard from './railcard/Railcard';
 
-class App extends Component {
-  render() {
-    return (
-      <Hiro />
-      // <Torus />
-      // <Earth />
-      // <RollingScopes />
-      // <TrainTicket />
-      // <Railcard />
+function App() {
+  return(
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <StoreProvider>
+            <Nav />
+            <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/products/ar" component={Hiro} />
+            </Switch>
+          </StoreProvider>
+        </div>
+      </Router>
+      </ApolloProvider>
     );
   }
-}
+
 
 render(<App />, document.getElementById('root'));
 
---
+++ client/src/components/CartItem/index.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { useStoreContext } from '../../utils/GlobalState';
+import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
+import { idbPromise } from '../../utils/helpers';
+
+// expects item component as a prop and will use that object's properties to populate JSX
+const CartItem = ({ item }) => {
+
+  const [, dispatch] = useStoreContext();
+
+  const removeFromCart = item => {
+    dispatch({
+      type: REMOVE_FROM_CART,
+      _id: item._id
+    });
+    idbPromise('cart', 'delete', { ...item });
+  };
+  // allow users to manually edit the quantity of shopping cart items
+  // anytime an <input> element's value changes, an onChange event will occur
+  // capture that event and send the element's new value to the reducer
+  const onChange = (e) => {
+    const value = e.target.value;
+  
+    if (value === '0') {
+      dispatch({
+        type: REMOVE_FROM_CART,
+        _id: item._id
+      });
+
+      idbPromise('cart', 'delete', { ...item });
+    } else {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: item._id,
+        purchaseQuantity: parseInt(value)
+      });
+
+      idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
+    }
+  };
+
+  return (
+    <div className="flex-row">
+      <div>
+        <img
+          src={`/images/${item.image}`}
+          alt=""
+        />
+      </div>
+      <div>
+        <div>{item.name}, ${item.price}</div>
+        <div>
+          <span>Qty:</span>
+          <input
+            type="number"
+            placeholder="1"
+            value={item.purchaseQuantity}
+            onChange={onChange}
+          />
+          <span
+            role="img"
+            aria-label="trash"
+            onClick={() => removeFromCart(item)}
+          >
+            🗑️
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CartItem;
