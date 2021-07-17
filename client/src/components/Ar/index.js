ee7d92029ba36e8b957c1254b8597823b2dee020
Killa J <joshuaalexandercross@gmail.com>
2021-07-17 01:27:57 -0500

    fixing AR component

--- client/src/components/Ar/index.js
+++ client/src/components/Ar/index.js
@@ -1,256 +1,35 @@
-
-
-// import React from 'react';
-// import { useParams } from 'react-router-dom';
-// const { id } = useParams();
-// import arObjImage from "`./assets/${id}/obj-${id}.png`";
-// import arMtlImage from "`./assets/${id}/mtl-${id}.png`";
-// import { AFrameRenderer, Marker } from 'react-web-ar';
-
-// function Ar () {
-//   const objID = `${id}-obj`
-//   const mtlID = `${id}-mtl`
-
-//     return (
-//       <AFrameRenderer arToolKit={{ sourceType: 'webcam' }} >
-//         <Marker parameters={{ preset: 'hiro' }}>
-//           <a-assets>
-//             <a-assets-item id={objID} src={arObjImage}></a-assets-item>
-//             <a-assets-item id={mtlID} src={arMtlImage}></a-assets-item>
-//           </a-assets>
-//           <a-entity obj-model="obj: #{objID}; mtl: #{mtlID}"></a-entity>
-//         </Marker>
-//       </AFrameRenderer>
-//     );
-// }
-
-// export default Ar;
-
-
-
+import { AFrameRenderer, Marker } from 'react-web-ar';
 import React from 'react';
-// import { AFrameRenderer, Marker } from 'react-web-ar';
-// import arObjImage from "./assets/60f225322ff0629053973ccc/60f225322ff0629053973ccc.obj";
-// import arMtlImage from "./assets/60f225322ff0629053973ccc/60f225322ff0629053973ccc.mtl";
-// import { 
-//   ObjectLoader, 
-//   MaterialLoader, 
-//   arToolkitSource, 
-//   arToolkitContext, 
-//   artoolkitcontrols, 
-//   ArMarkerControls, 
-//   arbasecontrols 
-// } from 'three';
-<a-scene
-  ...
-  xrextras-pwa-installer
-  xrweb></a-scene>
+import { useParams } from 'react-router-dom';
+
+// ultimately, I want to have one component that has temporal literals that dynamically change the id based on the QR code.
+
+// going to have to use a query to grab ID and insert, because you cannot use
+// temporal literals in imports
+
+// also received this error:
+// aframe-ar.js:3379 Uncaught TypeError: Cannot read property 'projectionMatrix' of null
+//     at ARjs.Source.THREEx.ArToolkitSource.ARjs.Source.onResize (aframe-ar.js:3379)
 
 function Ar () {
+  const { id } = useParams();
+  const objID = `${id}-obj`
+  const mtlID = `${id}-mtl`
 
     return (
-      <a-scene
-        xrextras-gesture-detector
-        xrextras-almost-there
-        xrextras-loading
-        xrextras-runtime-error
-        xrextras-pwa-installer
-        renderer="colorManagement: true"
-        xrweb>
-      
-        <a-assets>
-          <a-asset-item id="toyModel" src="assets/toy.glb"></a-asset-item>
-        </a-assets>
-      
-        <a-camera
-          id="camera"
-          position="0 8 8"
-          raycaster="objects: .cantap"
-          cursor="fuse: false; rayOrigin: mouse;">
-        </a-camera>
-      
-        <a-entity
-          light="
-            type: directional;
-            intensity: 0.8;
-            castShadow: true;
-            shadowMapHeight:2048;
-            shadowMapWidth:2048;
-            shadowCameraTop: 10;
-            target: #model;"
-          xrextras-attach="target: model; offset: 1 15 3;"
-          shadow>
-        </a-entity>
-      
-        <a-light type="ambient" intensity="0.7"></a-light>
-      
-        <a-entity 
-          id="model" 
-          gltf-model="#toyModel" 
-          class="cantap" 
-          xrextras-hold-drag 
-          xrextras-two-finger-rotate 
-          xrextras-pinch-scale 
-          scale="1 1 1" 
-          shadow="receive: false">
-        </a-entity>
-      
-        <a-plane
-          id="ground" 
-          rotation="-90 0 0" 
-          width="1000" 
-          height="1000" 
-          material="shader: shadow" 
-          shadow>
-        </a-plane>
-      </a-scene>
+      <AFrameRenderer arToolKit={{ sourceType: 'webcam' }} >
+        <Marker parameters={{ preset: 'hiro' }}>
+          <a-scene>
+          <a-assets>
+            <a-assets-item id={objID} src="./assets/60f225322ff0629053973ccc/60f225322ff0629053973ccc.obj"></a-assets-item>
+            <a-assets-item id={mtlID} src="./assets/60f225322ff0629053973ccc/60f225322ff0629053973ccc.obj"></a-assets-item>
+          </a-assets>
+          
+          <a-entity obj-model="obj: #{objID}; mtl: #{mtlID}"></a-entity>
+          </a-scene>
+        </Marker>
+      </AFrameRenderer>
     );
 }
 
 export default Ar;
-
-
-
-
-
-// var scene, camera, renderer, clock, deltaTime, totalTime;
-
-// var arToolkitSource, arToolkitContext;
-
-// var markerRoot1;
-
-// var mesh1;
-
-// initialize();
-// animate();
-
-// function initialize()
-// {
-// 	scene = new THREE.Scene();
-
-// 	let ambientLight = new THREE.AmbientLight( 0xcccccc, 1.0 );
-// 	scene.add( ambientLight );
-				
-// 	camera = new THREE.Camera();
-// 	scene.add(camera);
-
-// 	renderer = new THREE.WebGLRenderer({
-// 		antialias : true,
-// 		alpha: true
-// 	});
-// 	renderer.setClearColor(new THREE.Color('lightgrey'), 0)
-// 	renderer.setSize( 640, 480 );
-// 	renderer.domElement.style.position = 'absolute'
-// 	renderer.domElement.style.top = '0px'
-// 	renderer.domElement.style.left = '0px'
-// 	document.body.appendChild( renderer.domElement );
-
-// 	clock = new THREE.Clock();
-// 	deltaTime = 0;
-// 	totalTime = 0;
-	
-// 	////////////////////////////////////////////////////////////
-// 	// setup arToolkitSource
-// 	////////////////////////////////////////////////////////////
-
-// 	arToolkitSource = new THREEx.ArToolkitSource({
-// 		sourceType : 'webcam',
-// 	});
-
-// 	function onResize()
-// 	{
-// 		arToolkitSource.onResize()	
-// 		arToolkitSource.copySizeTo(renderer.domElement)	
-// 		if ( arToolkitContext.arController !== null )
-// 		{
-// 			arToolkitSource.copySizeTo(arToolkitContext.arController.canvas)	
-// 		}	
-// 	}
-
-// 	arToolkitSource.init(function onReady(){
-// 		onResize()
-// 	});
-	
-// 	// handle resize event
-// 	window.addEventListener('resize', function(){
-// 		onResize()
-// 	});
-	
-// 	////////////////////////////////////////////////////////////
-// 	// setup arToolkitContext
-// 	////////////////////////////////////////////////////////////	
-
-// 	// create atToolkitContext
-// 	arToolkitContext = new THREEx.ArToolkitContext({
-// 		cameraParametersUrl: 'data/camera_para.dat',
-// 		detectionMode: 'mono'
-// 	});
-	
-// 	// copy projection matrix to camera when initialization complete
-// 	arToolkitContext.init( function onCompleted(){
-// 		camera.projectionMatrix.copy( arToolkitContext.getProjectionMatrix() );
-// 	});
-
-// 	////////////////////////////////////////////////////////////
-// 	// setup markerRoots
-// 	////////////////////////////////////////////////////////////
-
-// 	// build markerControls
-// 	markerRoot1 = new THREE.Group();
-// 	scene.add(markerRoot1);
-// 	let markerControls1 = new THREEx.ArMarkerControls(arToolkitContext, markerRoot1, {
-// 		type: 'pattern', patternUrl: "data/hiro.patt",
-// 	})
-
-// 	let geometry1 = new THREE.PlaneBufferGeometry(1,1, 4,4);
-// 	let loader = new THREE.TextureLoader();
-// 	// let texture = loader.load( 'images/earth.jpg', render );
-// 	let material1 = new THREE.MeshBasicMaterial( { color: 0x0000ff, opacity: 0.5 } );
-// 	mesh1 = new THREE.Mesh( geometry1, material1 );
-// 	mesh1.rotation.x = -Math.PI/2;
-// 	markerRoot1.add( mesh1 );
-	
-// 	function onProgress(xhr) { console.log( (xhr.loaded / xhr.total * 100) + '% loaded' ); }
-// 	function onError(xhr) { console.log( 'An error happened' ); }
-	
-// 	new THREE.MTLLoader()
-// 		.setPath( 'models/' )
-// 		.load( 'fish-2.mtl', function ( materials ) {
-// 			materials.preload();
-// 			new THREE.OBJLoader()
-// 				.setMaterials( materials )
-// 				.setPath( 'models/' )
-// 				.load( 'fish-2.obj', function ( group ) {
-// 					mesh0 = group.children[0];
-// 					mesh0.material.side = THREE.DoubleSide;
-// 					mesh0.position.y = 0.25;
-// 					mesh0.scale.set(0.25,0.25,0.25);
-// 					markerRoot1.add(mesh0);
-// 				}, onProgress, onError );
-// 		});
-// }
-
-
-// function update()
-// {
-// 	// update artoolkit on every frame
-// 	if ( arToolkitSource.ready !== false )
-// 		arToolkitContext.update( arToolkitSource.domElement );
-// }
-
-
-// function render()
-// {
-// 	renderer.render( scene, camera );
-// }
-
-
-// function animate()
-// {
-// 	requestAnimationFrame(animate);
-// 	deltaTime = clock.getDelta();
-// 	totalTime += deltaTime;
-// 	update();
-// 	render();
-// }
-
