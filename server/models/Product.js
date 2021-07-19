3226f901e044200e7e7f3bcad9149ff480c7222b
JPB <jbanas9124@gmail.com>
2021-07-19 15:56:07 -0500

    Product

---
+++ server/models/Product.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String
+  },
+  modelImage: {
+    type: String
+  },
+  thumbnail: {
+    type: String
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0.99
+  },
+  quantity: {
+    type: Number,
+    min: 0,
+    default: 0
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true
+  },
+  reviews: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Review',
+  }]
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
