db2f8153412b7fb542a34b8f812c584c3ce79875
JPB <jbanas9124@gmail.com>
2021-07-22 17:14:15 -0500

    seeds save

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -21,8 +21,8 @@ db.once('open', async () => {
       name: 'Cool Watch One',
       description:
         'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
-      modelImage: 'watchone.jpg',
-      thumbnail: 'watchone.jpg',
+      modelImage: 'watchone.jpeg',
+      thumbnail: 'watchone.jpeg',
       category: categories[0]._id,
       price: 2.99,
       quantity: 500,
@@ -32,8 +32,8 @@ db.once('open', async () => {
       name: 'Hat One',
       description:
         'Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.',
-      modelImage: 'hatone.jpg',
-      thumbnail: 'hatone.jpg',
+      modelImage: 'hatone.jpeg',
+      thumbnail: 'hatone.jpeg',
       category: categories[1]._id,
       price: 1.99,
       quantity: 500,
@@ -44,8 +44,8 @@ db.once('open', async () => {
       category: categories[2]._id,
       description:
         'Donec volutpat erat erat, sit amet gravida justo sodales in. Phasellus tempus euismod urna. Proin ultrices nisi ut ipsum congue, vitae porttitor libero suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam lacinia a nisi non congue.',
-      modelImage: 'sunone.jpg',
-      thumbnail: 'sunone.jpg',
+      modelImage: 'sunone.jpeg',
+      thumbnail: 'sunone.jpeg',
       price: 7.99,
       quantity: 20,
       reviews: []
@@ -55,8 +55,8 @@ db.once('open', async () => {
       category: categories[0]._id,
       description:
         'Praesent placerat, odio vel euismod venenatis, lectus arcu laoreet felis, et fringilla sapien turpis vestibulum nisl.',
-      modelImage: 'watchtwo.jpg',
-      thumbnail: 'watchtwo.jpg',
+      modelImage: 'watchtwo.jpeg',
+      thumbnail: 'watchtwo.jpeg',
       price: 3.99,
       quantity: 50,
       reviews: []
@@ -66,8 +66,8 @@ db.once('open', async () => {
       category: categories[1]._id,
       description:
         'Vivamus ut turpis in purus pretium mollis. Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
-      modelImage: 'hattwo.jpg',
-      thumbnail: 'hattwo.jpg',
+      modelImage: 'hattwo.jpeg',
+      thumbnail: 'hattwo.jpeg',
       price: 14.99,
       quantity: 100,
       reviews: []
@@ -77,8 +77,8 @@ db.once('open', async () => {
       category: categories[2]._id,
       description:
         'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
-      modelImage: 'suntwo.jpg',
-      thumbnail: 'suntwo.jpg',
+      modelImage: 'suntwo.jpeg',
+      thumbnail: 'suntwo.jpeg',
       price: 399.99,
       quantity: 30,
       reviews: []
@@ -88,8 +88,8 @@ db.once('open', async () => {
       category: categories[0]._id,
       description:
         'In sodales, ipsum quis ultricies porttitor, tellus urna aliquam arcu, eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris justo ante, aliquet non tempus in, tempus ac lorem. Aliquam lacinia dolor eu sem eleifend ultrices. Etiam mattis metus metus. Sed ligula dui, placerat non turpis vitae, suscipit volutpat elit. Phasellus sagittis, diam elementum suscipit fringilla, libero mauris scelerisque ex, ac interdum diam erat non sapien.',
-      modelImage: 'watchthree.jpg',
-      thumbnail: 'watchthree.jpg',
+      modelImage: 'watchthree.jpeg',
+      thumbnail: 'watchthree.jpeg',
       price: 199.99,
       quantity: 30,
       reviews: []
@@ -99,8 +99,8 @@ db.once('open', async () => {
       category: categories[1]._id,
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.',
-      modelImage: 'hatthree.jpg',
-      thumbnail: 'hatthree.jpg',
+      modelImage: 'hatthree.jpeg',
+      thumbnail: 'hatthree.jpeg',
       price: 9.99,
       quantity: 100,
       reviews: []
@@ -109,8 +109,8 @@ db.once('open', async () => {
       name: 'Sunnies Three',
       category: categories[2]._id,
       description: 'Ut vulputate hendrerit nibh, a placerat elit cursus interdum.',
-      modelImage: 'sunthree.jpg',
-      thumbnail: 'sunthree.jpg',
+      modelImage: 'sunthree.jpeg',
+      thumbnail: 'sunthree.jpeg',
       price: 1.99,
       quantity: 1000,
       reviews: []
@@ -120,8 +120,8 @@ db.once('open', async () => {
       category: categories[0]._id,
       description:
         'Sed a mauris condimentum, elementum enim in, rhoncus dui. Phasellus lobortis leo odio, sit amet pharetra turpis porta quis.',
-      modelImage: 'watchfour.jpg',
-      thumbnail: 'watchfour.jpg',
+      modelImage: 'watchfour.jpeg',
+      thumbnail: 'watchfour.jpeg',
       price: 2.99,
       quantity: 1000,
       reviews: []
@@ -131,8 +131,8 @@ db.once('open', async () => {
       category: categories[1]._id,
       description:
         'Vestibulum et erat finibus erat suscipit vulputate sed vitae dui. Ut laoreet tellus sit amet justo bibendum ultrices. Donec vitae felis vestibulum, congue augue eu, finibus turpis.',
-      modelImage: 'hatfour.jpg',
-      thumbnail: 'hatfour.jpg',
+      modelImage: 'hatfour.jpeg',
+      thumbnail: 'hatfour.jpeg',
       price: 7.99,
       quantity: 100,
       reviews: []
@@ -142,8 +142,8 @@ db.once('open', async () => {
       category: categories[2]._id,
       description:
         'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
-      modelImage: 'sunfour.jpg',
-      thumbnail: 'sunfour.jpg',
+      modelImage: 'sunfour.jpeg',
+      thumbnail: 'sunfour.jpeg',
       price: 9.99,
       quantity: 600,
       reviews: []
