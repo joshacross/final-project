f1ad1be776caff852ce52f1a0da85c95b49cb295
okni-c <oknistay@gmail.com>
2021-07-25 20:46:13 -0500

    added new nav bar for better functionality

---
+++ client/src/components/NewNav/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ShoppingCart from '@material-ui/icons/ShoppingCartOutlined'
+
+
+function NewNav() {
+
+
+
+    return (
+        <nav>
+            <div class="nav-wrapper">
+                <a href="#" class="brand-logo">Logo</a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><a href="sass.html">Sass</a></li>
+                    <li><a href="badges.html">Components</a></li>
+                    <li><a href="collapsible.html">JavaScript</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default NewNav;
