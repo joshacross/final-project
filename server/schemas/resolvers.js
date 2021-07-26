19b41cba7c81d20469ca1120c0ea495e096700ca
Mediogre <caseyarrington13@gmail.com>
2021-07-25 21:42:28 -0500

    adjusted resolvers and auth to protect secret and resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,7 +2,7 @@ const { AuthenticationError } = require("apollo-server-express");
 const { User, Category, Product, Order, Review } = require("../models");
 const { signToken } = require("../utils/auth");
 require('dotenv').config();
-const stripe = require("stripe")('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+const stripe = require("stripe")(process.env.STRIPE_API_KEY);
 
 
 const resolvers = {
